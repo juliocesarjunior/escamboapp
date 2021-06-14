957d7840d9e1c95ea1e3dca930afc67d3e8634a5
Julio Cesar Junior <julio.cesar3151@hotmail.com.br>
2021-06-14 07:38:42 -0300

    Atualização

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  
+  layout :layout_by_resource
+
+  protected
+
+    def layout_by_resource
+      if devise_controller? && resource_name == :admin
+        "backoffice_devise"
+      elsif devise_controller? && resource_name == :member
+        "site_devise"
+      else
+        "application"
+      end
+    end
 end
---
+++ app/controllers/backoffice/categories_controller.rb
@@ -0,0 +1,23 @@
+class Backoffice::CategoriesController < BackofficeController
+    def index
+  	  	@categories = Category.all
+  end
+
+  def new
+  	
+  end
+
+  def create
+  	
+  end
+
+  def edit
+  	
+  end
+
+  def uptade
+  	
+  end
+
+
+end
